052b001dcdaaf5e0aa1908d9a4b20e61948400ea
ShubhamMore07 <psmore4u@gmail.com>
2024-08-11 17:55:07 +0530

    Add files via upload

---
+++ First.js
@@ -0,0 +1,31 @@
+//Datatypes
+//Premetive Datatypes
+//number
+var a = 30;
+
+//string
+var uname = "Shubham";
+
+//boolean
+var bl = true;
+
+//null
+b ;
+//undefined
+var u;
+
+//Non-Premetive Datatypes
+//Array
+var ar = [30,20,87,54,65,26];
+var userNames = ["Shubham","Abhijit","Kunal","Soham"];
+var ar3 = [true, false, false, true, true, false];
+console.log(ar[1]);
+
+
+//object
+var obj =[
+    name1:"Shubham",
+    rollNo: 23,
+    MS:false,
+    sName:"More"
+];
